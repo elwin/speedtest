c15ef78c47ea04571685413c5c1d4619ade999db
Elwin Stephan <elwin.stephan@gmail.com>
2019-08-28 18:12:56 +0200

    Initial commit

---
+++ speedtest_client/speedtest_client.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"time"
+
+	"github.com/elwin/transmit2/scion"
+)
+
+var (
+	local  = flag.String("local", "", "Local address (with Port)")
+	remote = flag.String("remote", "", "Remote address (with Port)")
+	size   = flag.Int("size", 1024, "KB to be sent")
+)
+
+const (
+	sizeMuliplier = 1024 * 1024 // MB
+)
+
+func main() {
+	flag.Parse()
+	if *local == "" {
+		log.Fatal("Please specify the local address using -local")
+	}
+
+	if *remote == "" {
+		log.Fatal("Please specify the remote address using -remote")
+	}
+
+	conn, err := scion.DialAddr(*local, *remote, scion.DefaultPathSelector)
+	if err != nil {
+		log.Fatal("failed to connect", err)
+	}
+	defer conn.Close()
+
+	header := make([]byte, 4)
+	binary.PutUvarint(header, uint64(*size*sizeMuliplier))
+
+	start := time.Now()
+
+	if err := binary.Write(conn, binary.BigEndian, header); err != nil {
+		log.Fatal("failed to write size", err)
+	}
+
+	if n, err := io.CopyN(ioutil.Discard, conn, int64(*size)*sizeMuliplier); err != nil && n != int64(*size)*sizeMuliplier {
+		log.Fatal("failed to read payload", err)
+	} else {
+		fmt.Printf("read %d MB\n", *size)
+	}
+
+	fmt.Println(float64(*size)/time.Since(start).Seconds(), " MB/s")
+
+}
---
+++ speedtest_server/speedtest_server.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/elwin/transmit2/scion"
+)
+
+var (
+	local = flag.String("local", "", "Local address (with Port)")
+)
+
+func main() {
+	flag.Parse()
+	if *local == "" {
+		log.Fatal("Please specify the local address using -local")
+	}
+
+	listener, err := scion.Listen(*local)
+	if err != nil {
+		log.Fatal("failed to listen", err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("failed to accept connection", err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn *scion.Connection) {
+	defer conn.Close()
+	header := make([]byte, 4)
+	if err := binary.Read(conn, binary.BigEndian, &header); err != nil {
+		fmt.Println("failed to read header", err)
+		return
+	}
+
+	length, err := binary.ReadUvarint(bytes.NewReader(header))
+	if err != nil {
+		fmt.Println("failed to read header", err)
+		return
+	}
+
+	if n, err := io.CopyN(conn, rand.Reader, int64(length)); err != nil {
+		fmt.Println("failed to send payload", err)
+	} else {
+		fmt.Printf("wrote %d bytes\n", n)
+	}
+}
